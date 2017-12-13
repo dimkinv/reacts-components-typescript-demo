1f0b11e2e03f96cc90741a8e1a262a796bfdb947
Yonathan Benolol <yb948g@intl.att.com>
2017-12-13 13:08:01 +0200

    added card

---
+++ components/card/Card.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import * as glamor from 'glamor';
+import glamorous from 'glamorous';
+import CardProps from './CardProps';
+import CardState from './CardState';
+import './Card.css';
+
+export default class Card extends React.Component<CardProps, CardState>{
+  constructor(props: CardProps){
+    super(props);
+  }
+    render(){
+        const Title = glamorous.h1({
+          fontSize: '24px',
+          color: 'tomato',
+          ':hover': {
+            color: 'limegreen'
+          }
+        });
+        // Creating the animation
+        const animationStyles = () => {
+          const bounce = glamor.css.keyframes('bounce',{
+            '0%': { transform: `scale(1.01)`},
+            '100%': { transform: `scale(0.99)`}
+          })
+          return { animation: `${bounce} 0.5s infinite ease-in-out alternate`};
+        }
+        // Composing animation with the title
+        const AnimatedTitle = glamorous(Title)(animationStyles);
+
+        const Content = glamorous.p({
+          fontWeight: 400,
+          color: 'lightgrey',
+        });
+
+        return (
+          <div>
+            <AnimatedTitle>{this.props.title}</AnimatedTitle>
+            <Content>{this.props.content}</Content>
+          </div>
+        )
+    }
+}
--- components/label/Label.tsx
+++ components/label/Label.tsx
@@ -5,7 +5,7 @@ import LabelState from './LabelState';
 export default class Label extends React.Component<LabelProps, LabelState>{
     render(){
         const styles = {
-          color: 'red',
+          color: 'green',
           fontSize: '24px'
         };
         return (<span style={styles}>{this.props.text}</span>)
